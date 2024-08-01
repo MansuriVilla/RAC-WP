201146c18b1fb254769eb7a703a4a29de9de8ee7
Mansuri Villa <113539390+MansuriVilla@users.noreply.github.com>
2024-08-01 18:39:51 +0530

    Video Section and Review Slider Added

--- js/app.js
+++ js/app.js
@@ -17,5 +17,22 @@ $(document).ready(function () {
       },
     });
   });
+$(document).ready(function () {
+    $(".slider-default-review").owlCarousel({
+      loop: true,
+      nav: true,
+      dots: false,
+      autoWidth:true,
+      navText: ["<img src='./assets/icons/slider-arrow-left.svg'>","<img src='./assets/icons/slider-arrow-right.svg'>"],
+      responsive: {
+        576: {
+          items: 1,
+        },
+        1000: {
+          items: 2.3,
+        },
+      },
+    });
+  });
 
   
